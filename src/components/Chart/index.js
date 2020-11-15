acadc16e98f50ae541070843d2c4e86cb3f6ba30
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-11-15 11:09:38 +0700

    responsive undone

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -12,9 +12,9 @@ class Chart extends Component {
                     label: 'Covid19',
                     data: data,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)'
+                        'yellow',
+                        'blue',
+                        'red'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -32,7 +32,7 @@ class Chart extends Component {
             <div className="chart-wrapper">
                 <Bar data={this.state.dataChart}
                     options={{ maintainAspectRatio: false }}
-                    height={300}
+                    height={500}
                  />
             </div>
         )
--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -3,8 +3,10 @@ import './Dashboard.css';
 import axios from 'axios';
 import Chart from '../../components/Chart';
 import Headbar from '../../components/Headbar';
-import Countries from '../../components/Countries';
+// import Countries from '../../components/Countries';
 import CountryDetail from '../../components/CountryDetail';
+import ArraySort from 'array-sort';
+import HeadCountries from '../../components/HeadCountries';
 
 class Dashboard extends Component {
     state = {
@@ -12,67 +14,113 @@ class Dashboard extends Component {
         recovered: 0,
         deaths: 0,
         activeCase: 0,
-        isLoading: false
+        isLoading: false,
+        countryDetails: [],
+        searchedCountry: []
     }
 
     async componentDidMount() {
-        // axios.get('https://covid19.mathdro.id/api')
-        //     .then(res => {
-        //         console.log(res.data)
-        //         this.setState({
-        //             confirmed: res.data.confirmed.value,
-        //             recovered: res.data.recovered.value,
-        //             deaths: res.data.deaths.value,
-        //             isLoading: true
-        //         })
-        //     })
         const globalData = await axios.get('https://api.covid19api.com/summary');
         let covid = globalData.data.Global
+        let countryDetails = globalData.data.Countries
+        countryDetails = ArraySort(countryDetails, 'TotalConfirmed', {reverse: true});
         
         this.setState({
             confirmed: covid.TotalConfirmed,
             recovered: covid.TotalRecovered,
             deaths: covid.TotalDeaths,
             activeCase: covid.TotalConfirmed - (covid.TotalRecovered + covid.TotalDeaths),
-            isLoading: true
+            isLoading: true,
+            countryDetails: countryDetails,
+            status: true,
+            selectedData: countryDetails
         })
     }
 
+    ChangeSortValue = e => {
+        const value = e.target.value;
+        let sortByReverse = true;
+
+        if(value === "Highest") {
+            sortByReverse = true
+        } else {
+            sortByReverse = false
+        }
+
+        let countryDetails = ArraySort(this.state.countryDetails, 'TotalConfirmed', {reverse: sortByReverse})
+
+        this.setState({countryDetails: countryDetails, status: true})
+    }
+
+    searchCountry = e => {
+        const value = e.target.value;
+        
+        const countryDetails = this.state.countryDetails
+
+        let findCountry =  []
+
+        if(value) {
+            countryDetails.map(function(cur, index) {
+                const finder = cur.Country.toLowerCase().search(value.toLowerCase())
+                if(finder !== -1) {
+                    findCountry.push(countryDetails[index])
+                }
+                return findCountry  // ??
+            })
+
+            findCountry = ArraySort(findCountry, 'TotalConfirmed', {reverse: true})
+            this.setState({searchedCountry: findCountry})
+
+        } else {
+            this.setState({countryDetails: countryDetails})
+        }
+
+        if(value.length === 0) {
+            this.setState({selectedData: this.state.countryDetails})
+        } else {
+            this.setState({selectedData: this.state.searchedCountry})
+        }
+    }
+
     render() {
-        const {confirmed, recovered, deaths, activeCase,isLoading} = this.state;
+        let countriesList = this.state.countryDetails.length > 0 ?
+        this.state.selectedData.map(function(cur, index) {
+            return <CountryDetail
+                        key={index}
+                        countryCode={cur.CountryCode}
+                        totalCases={cur.TotalConfirmed}
+                        newCases={cur.NewConfirmed}
+                        totalDeaths={cur.TotalDeaths}
+                        newDeaths={cur.NewDeaths}
+                        totalRecovered={cur.TotalRecovered}
+                        newRecovered={cur.NewRecovered}
+                    />
+        }) : null
+        const {confirmed, recovered, deaths, activeCase, isLoading} = this.state;
         return (
             <div className="container">
                 <Headbar confirm={confirmed} recover={recovered} death={deaths} active={activeCase} />
-                {/* <div className="world-stats">
-                    <div className="cases">
-                        <div>
-                            <h3>Confirmed</h3>
-                            {confirmed}
-                        </div>
-                        <div>
-                            <h3>Recovered</h3>
-                            {recovered}
-                        </div>
-                        <div>
-                            <h3>Deaths</h3>
-                            {deaths}
-                        </div>
-                    </div>
-                </div> */}
-                <Countries />
-                <CountryDetail
-                    countryCode="IN"
-                    totalCases="4444"
-                    newCases="33"
-                    totalDeaths="1233"
-                    newDeaths="123"
-                    totalRecovered="22"
-                    newRecovered="11"
-                />
-                {/* {
-                    isLoading ?  <Chart value={isLoading} labels={['Confirmed', 'Recovered', 'Deaths']} data={[confirmed, recovered, deaths]}/>
+                {/* <Countries /> */}
+
+                {
+                    isLoading ?  <Chart value={isLoading} labels={['Confirmed', 'Recovered', 'Deaths', 'Active Case']} data={[confirmed, recovered, deaths, activeCase]}/>
                     : null                   
-                } */}
+                }
+
+                <div className="countries-stats">
+            
+                    <h2 className="countries-heading">Countries Stats</h2>
+
+                    <div className="filtering">
+                        <input type="text" placeholder="Enter Country Name" onChange={this.searchCountry} />
+                        <select className="sort-by" onChange={this.ChangeSortValue}>
+                            <option>Highest</option>
+                            <option>Lowest</option>
+                        </select>
+                    </div>
+                    <HeadCountries />
+                </div>
+                {countriesList}
             </div>
         )
     }
